185ed4b84b6be5687dd7c8b14127bb109735ecce
SwithunHan <18833567817@163.com>
2019-04-05 18:09:20 +0800

    建立数据库模型

--- apps/house/views.py
+++ apps/house/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from rest_framework import viewsets, mixins
+
 
 # Create your views here.
+class HouseViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    pass
