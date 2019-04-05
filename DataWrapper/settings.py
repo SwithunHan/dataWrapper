da6175c20ddbb9410b0334923620cc7c0340b1fe
SwithunHan <18833567817@163.com>
2019-04-05 18:12:40 +0800

    初始化爬虫功能

--- DataWrapper/settings.py
+++ DataWrapper/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
@@ -27,7 +26,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -38,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'house.apps.HouseConfig'
 ]
 
 MIDDLEWARE = [
@@ -71,7 +70,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'DataWrapper.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
@@ -85,7 +83,6 @@ DATABASES = {
         'OPTIONS': {'init_command': "SET default_storage_engine=INNODB;"}
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
@@ -105,7 +102,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
@@ -119,8 +115,9 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+CITY = 'bj'  # only one, shanghai=sh shenzhen=sh......
+REGIONLIST = [u'fengtai', u'tongzhou']  # only pinyin support
---
+++ apps/utils/scrawl.py
@@ -0,0 +1,23 @@
+from utils import core
+from house import models
+
+from DataWrapper.settings import REGIONLIST, CITY
+
+
+def get_communitylist(city):
+    res = []
+    for community in models.Community.select():
+        if community.city == city:
+            res.append(community.title)
+    return res
+
+
+if __name__ == "__main__":
+    regionlist = REGIONLIST  # only pinyin support
+    city = CITY
+    core.GetHouseByRegionlist(city, regionlist)
+    core.GetRentByRegionlist(city, regionlist)
+    # Init,scrapy celllist and insert database; could run only 1st time
+    core.GetCommunityByRegionlist(city, regionlist)
+    communitylist = get_communitylist(city)  # Read celllist from database
+    core.GetSellByCommunitylist(city, communitylist)
